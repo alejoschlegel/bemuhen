2d20ed73691e87698525969c8f8bcfdbe6d24633
schale29 <alejofschlegel@gmail.com>
2022-04-18 16:18:12 -0300

    Create-react-app command

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        doloremque, quidem, quisquam quisquam doloremque quidem quisquam
+        doloremque.
+      </p>
+    </div>
+  );
+}
